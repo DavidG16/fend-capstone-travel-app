aad03ba11acc346a1d9a5ef7aa2034f8409dd141
David Grijalva <dgrijalva@indeed.com>
2020-04-05 12:23:29 -0500

    Changes to src client and server

---
+++ src/server/app.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+  }))
+
+module.exports = app;
---
+++ src/server/server.js
@@ -0,0 +1,5 @@
+const app = require('./app')
+port = 3000
+
+app.listen(port, ()=> console.log(`App is listening at port ${port}`))
+

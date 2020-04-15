a2d25fb7aa3b8b9c99150a6cb898191eb89420ce
David Grijalva <dgrijalva@indeed.com>
2020-04-14 21:23:55 -0500

    changes

--- src/client/js/getData.js
+++ src/client/js/getData.js
@@ -4,17 +4,26 @@ import axios from 'axios';
     const url=  "http://api.geonames.org/searchJSON?q=",
     completeURL = `${url}${city}&username=${username}`
     console.log(completeURL)
-    let data;
+ 
 
-    await axios.get(completeURL).then((response) => {
-        console.log(response.data);
-        console.log(response.status);
-        console.log(response.statusText);
-        console.log(response.headers);
-        console.log(response.config);
-        data = response;
-      });
-      return data;
+    try {
+        let data = await axios.get(completeURL).then((response) => {
+            console.log(response.data);
+            console.log(response.status);
+            console.log(response.statusText);
+            console.log(response.headers);
+            console.log(response.config);
+            data = response;
+          });
+          return data;
+
+    }
+    catch(error) {
+        console.log("error", error);
+        // appropriately handle the error
+      }
+
+ 
 
 }
 
